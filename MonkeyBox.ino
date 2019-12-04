df749c99921b4c2a32f3b6b279a947e3bbc877d4
Jochen Derwae <jochen.derwae@gmail.com>
2019-12-04 15:02:55 +0100

    Addded another motor Added a "subState" to handle animations within a state Added some basic animations

--- MonkeyBox.ino
+++ MonkeyBox.ino
@@ -7,10 +7,23 @@
 #define DFPLAYER_TX_PIN   4
 #define ARM_SERVO_PIN     5
 #define LID_SERVO_PIN     3
+#define DOOR_SERVO_PIN    9
 #define SWITCH_PIN        8
+
+#define DOOR_SERVO_MIN    0
+#define DOOR_SERVO_MAX    180
+#define LID_SERVO_MIN     0
+#define LID_SERVO_MID1    60
+#define LID_SERVO_MID2    120
+#define LID_SERVO_MAX     180
+#define ARM_SERVO_MIN     0
+#define ARM_SERVO_MAX     180
 
 #define MOOD_COOLDOWN_TIME 30000
 
+#define sgn(x) ((x) < 0 ? -1 : ((x) > 0 ? 1 : 0))
+#define second(x) (x * 1000)
+#define minute(x) (x * 60000)
 
 enum MonkeyMood {
   moodStarting = -1,
@@ -29,19 +42,31 @@ enum MonkeyMood {
 
 Servo armServo;
 Servo lidServo;
+Servo doorServo;
 DFPlayerMini soundPlayer;
 int armPos = 0;
+int lidPos = 0;
+int armSpeed = 0;
+int lidSpeed = 0;
+int lidTarget = 0;
+
 MonkeyMood monkeyMood = moodStarting;
+int subState = 0;
+unsigned long subTime;
 unsigned long moodChangeTime;
 bool switchOn = false;
 bool prevSwitchOn = false;
-int armSpeed = 0;
 
 void setup() {
   Serial.begin(9600);
 
   armServo.attach(ARM_SERVO_PIN);
   lidServo.attach(LID_SERVO_PIN);
+  doorServo.attach(DOOR_SERVO_PIN);
+
+  armServo.write(armPos);
+  lidServo.write(armPos);
+  doorServo.write(DOOR_SERVO_MIN);
 
   pinMode(SWITCH_PIN, INPUT);
   
@@ -72,6 +97,7 @@ void soundLoop() {
     if(switchOn) {
       moodChangeTime = currentTime;
       monkeyMood = (int)monkeyMood + 1;
+      subState = 0;
       if(monkeyMood > moodFurious) {
         monkeyMood = moodFurious;
       }
@@ -84,50 +110,71 @@ void soundLoop() {
   doTick();
   
   armPos += armSpeed;
-  if(armPos > 180) {
-    armPos = 180;
+  if(armPos > ARM_SERVO_MAX) {
+    armPos = ARM_SERVO_MAX;
   }
-  if(armPos < 0) {
-    armPos = 0;
+  if(armPos < ARM_SERVO_MIN) {
+    armPos = ARM_SERVO_MIN;
+  }
+
+  int prevLidSign = sgn(lidTarget - lidPos);
+  lidPos += lidSpeed;
+  if(lidPos > ARM_SERVO_MAX) {
+    lidPos = ARM_SERVO_MAX;
+  }
+  if(lidPos < ARM_SERVO_MIN) {
+    lidPos = ARM_SERVO_MIN;
+  }
+  int lidSign = sgn(lidTarget - lidPos);
+  if(lidSign != prevLidSign) {
+    lidPos = lidTarget;
+    lidSpeed = 0;
   }
 
   /*Serial.print("Arm Pos: ");
   Serial.print(armPos);
+  Serial.print(" Lid Pos: ");
+  Serial.print(lidPos);
+  Serial.print(" Lid Target: ");
+  Serial.print(lidTarget);
   Serial.print(" State: ");
-  Serial.println(monkeyMood);*/
-
+  Serial.print(monkeyMood);
+  Serial.println();*/
+  
   armServo.write(armPos);
-  lidServo.write(90);
+  lidServo.write(lidPos);
   delay(20);
 }
 
 void doSwitchedOn() {
+  doorServo.write(DOOR_SERVO_MAX);
   switch(monkeyMood) {
     case moodAsleep:
       armSpeed = 1;
       break;
     case moodSleepy:
       armSpeed = 2;
+      soundPlayer.playFile(0);
       break;
     case moodGrumpy:
       armSpeed = 3;
-      soundPlayer.playFile(0);
+      soundPlayer.playFile(1);
       break;
     case moodAnnoyed:
       armSpeed = 4;
-      soundPlayer.playFile(1);
+      soundPlayer.playFile(2);
       break;
     case moodSuspicious:
       armSpeed = 5;
-      soundPlayer.playFile(2);
+      soundPlayer.playFile(3);
       break;
     case moodWarning:
       armSpeed = 10;
-      soundPlayer.playFile(3);
+      soundPlayer.playFile(4);
       break;
     case moodAngry:
       armSpeed = 20;
-      soundPlayer.playFile(4);
+      soundPlayer.playFile(5);
       break;
     case moodFurious:
       armSpeed = 20;
@@ -137,51 +184,228 @@ void doSwitchedOn() {
 }
 
 void doSwitchedOff() {
+  doorServo.write(DOOR_SERVO_MIN);
   switch(monkeyMood) {
     case moodAsleep:
+      // normally this state isn't reachable
       armSpeed = -1;
       break;
     case moodSleepy:
-      armSpeed = -2;
+      armSpeed = -1;
       break;
     case moodGrumpy:
-      armSpeed = -3;
+      armSpeed = -2;
       break;
     case moodAnnoyed:
-      armSpeed = -5;
+      armSpeed = -3;
       break;
     case moodSuspicious:
-      armSpeed = -20;
-      break;
-    case moodWarning:
       armSpeed = -5;
       break;
-    case moodAngry:
+    case moodWarning:
       armSpeed = -20;
       break;
+    case moodAngry:
+      armSpeed = -5;
+      break;
     case moodFurious:
-      armSpeed = -10;
+      armSpeed = -20;
       break;
   }
 }
 
 void doTick() {
+  unsigned long timer = millis() - subTime;
   switch(monkeyMood) {
     case moodAsleep:
       break;
     case moodSleepy:
+      switch(subState) {
+        case 0:
+          lidTarget = LID_SERVO_MIN;
+          lidSpeed = -5;
+          subState++;
+          break;
+          
+        default:
+          break;
+      }
       break;
     case moodGrumpy:
+      switch(subState) {
+        case 0:
+          lidTarget = LID_SERVO_MID1;
+          lidSpeed = 5;
+          subState++;
+          subTime = millis();
+          break;
+          
+        case 1:
+          if(timer > second(3)) {
+            subState++;
+          }
+          break;
+
+        case 2:
+          lidTarget = LID_SERVO_MIN;
+          lidSpeed = -5;
+          subState++;
+          break;
+          
+        default:
+          break;
+      }
       break;
     case moodAnnoyed:
+      switch(subState) {
+        case 0:
+          lidTarget = LID_SERVO_MID2;
+          lidSpeed = 5;
+          subState++;
+          subTime = millis();
+          break;
+          
+        case 1:
+          if(timer > second(5)) {
+            subState++;
+          }
+          break;
+
+        case 2:
+          lidTarget = LID_SERVO_MIN;
+          lidSpeed = -5;
+          subState++;
+          break;
+          
+        default:
+          break;
+      }
       break;
     case moodSuspicious:
+      switch(subState) {
+        case 0:
+          lidTarget = LID_SERVO_MAX;
+          lidSpeed = 5;
+          subState++;
+          subTime = millis();
+          break;
+          
+        case 1:
+          if(timer > second(3)) {
+            subState++;
+          }
+          break;
+
+        case 2:
+          lidTarget = LID_SERVO_MIN;
+          lidSpeed = -5;
+          subState++;
+          break;
+          
+        default:
+          break;
+      }
       break;
     case moodWarning:
+      switch(subState) {
+        case 0:
+          lidTarget = LID_SERVO_MID2;
+          lidSpeed = 5;
+          subState++;
+          subTime = millis();
+          break;
+          
+        case 1:
+          if(timer > second(3)) {
+            subState++;
+          }
+          break;
+
+        case 2:
+          lidTarget = LID_SERVO_MIN;
+          lidSpeed = -5;
+          subState++;
+          subTime = millis();
+          break;
+          
+        case 3:
+          if(timer > second(3)) {
+            subState++;
+          }
+          break;
+        case 4:
+          lidTarget = LID_SERVO_MID2;
+          lidSpeed = 5;
+          subState++;
+          subTime = millis();
+          break;
+          
+        case 5:
+          if(timer > second(3)) {
+            subState++;
+          }
+          break;
+
+        case 6:
+          lidTarget = LID_SERVO_MIN;
+          lidSpeed = -5;
+          subState++;
+          subTime = millis();
+          break;
+          
+        default:
+          break;
+      }
       break;
     case moodAngry:
+      switch(subState) {
+        case 0:
+          lidTarget = LID_SERVO_MID1;
+          lidSpeed = 5;
+          subState++;
+          subTime = millis();
+          break;
+          
+        case 1:
+          if(timer > second(3)) {
+            subState++;
+          }
+          break;
+
+        case 2:
+          lidTarget = LID_SERVO_MIN;
+          lidSpeed = -5;
+          subState++;
+          break;
+          
+        default:
+          break;
+      }
       break;
     case moodFurious:
+      switch(subState) {
+        case 0:
+          lidTarget = LID_SERVO_MID1;
+          lidSpeed = 5;
+          subState++;
+          subTime = millis();
+          break;
+          
+        case 1:
+          if(timer > second(3)) {
+            subState++;
+          }
+          break;
+
+        case 2:
+          lidTarget = LID_SERVO_MIN;
+          lidSpeed = -5;
+          subState++;
+          break;
+          
+        default:
+          break;
+      }
       break;
   }
 }
